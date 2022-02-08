69768c81d4a860a223df8f122db4ecc851f2c9b8
Nghia Doan <58439905+katzenundhundle@users.noreply.github.com>
2022-02-08 23:42:43 +0700

    random a number

--- site/CodeStat.js
+++ site/CodeStat.js
@@ -1,6 +1,6 @@
 var http = require('http');
-
+var randomNumber = require('./lib/RandomNumber.js');
 http.createServer(function (req, res){
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World');
+    res.end('Hello World'+randomNumber.RandomNumber());
 }).listen(8080);
---
+++ site/lib/RandomNumber.js
@@ -0,0 +1,4 @@
+exports.RandomNumber = function(){
+    var randomNumber = Math.floor(Math.random() * 10);
+    return randomNumber;
+}
