5120df3ac68d663cc0e9ad1f9d950976db2ddac1
Nghia Doan <58439905+katzenundhundle@users.noreply.github.com>
2022-02-08 23:52:16 +0700

    url

--- site/CodeStat.js
+++ site/CodeStat.js
@@ -1,6 +1,9 @@
 var http = require('http');
+var url = require('url');
 var randomNumber = require('./lib/RandomNumber.js');
 http.createServer(function (req, res){
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World'+randomNumber.RandomNumber());
+    var q = url.parse(req.url, true).query;
+    var txt = q.year + " " + q.month
+    res.end(txt+'Hello World'+randomNumber.RandomNumber());
 }).listen(8080);
