20fc946cfbd11c461bc1380969203b643a192936
Nghia Doan <58439905+katzenundhundle@users.noreply.github.com>
2022-02-12 22:07:49 +0700

    oke

--- site/CodeStat.js
+++ site/CodeStat.js
@@ -1,22 +1,35 @@
-var express = require('express')
 
+var express = require('express');
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
 
-app.use(function(req, res){
-    res.type('text/plain');
-    res.status(404);
-    res.send('404 - Not Found');
+var handlebars = require('express3-handlebars')
+.create({ defaultLayout:'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', (process.env.PORT || 8080));
+
+//serve to the client without question
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+    res.render('home');
+   });
+
+app.get('/about', function(req, res){
+    res.render('about')
 });
-
+   //404 
+app.use(function(req, res){
+    res.status(404);
+    res.render('404');
+});
+//500
 app.use(function(err, req, res, next){
     console.error(err.stack);
-    res.type('text/plain');
     res.status(500);
-    res.send('500 - Server Error');
+    res.render('500');
 });
-
 app.listen(app.get('port'), function(){
-    console.log('Express started on http://localhost:' + app.get('port'));
+    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
